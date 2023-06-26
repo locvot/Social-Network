a5268937aa3c4d446b6a59e60f6acec8731d332d
ShadowGuns <vol6921@gmail.com>
2023-06-26 14:38:05 +0700

    add upload

--- twitter/src/index.ts
+++ twitter/src/index.ts
@@ -2,9 +2,12 @@ import express from 'express'
 import { defaultErrorHandler } from './middlewares/error.middlewares'
 import usersRouter from './routes/users.routes'
 import databaseService from './services/database.services'
+import mediasRouter from './routes/medias.routes'
+import { initFolder } from './utils/file'
 databaseService.connect()
 const app = express()
 const port = 3000
+initFolder()
 app.use(express.json())
 
 app.post('/', (req, res) => {
@@ -12,6 +15,7 @@ app.post('/', (req, res) => {
 })
 
 app.use('/users', usersRouter)
+app.use('/medias', mediasRouter)
 app.use(defaultErrorHandler)
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
