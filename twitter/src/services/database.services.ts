09195ade795b94784846adb2683dad01a690f92b
locvot <vol6921@gmail.com>
2023-07-14 15:41:36 +0700

    add hashtag

--- twitter/src/services/database.services.ts
+++ twitter/src/services/database.services.ts
@@ -4,6 +4,7 @@ import User from '../models/schemas/User.schema'
 import RefreshToken from '~/models/schemas/RefreshToeken.schema'
 import Follower from '~/models/schemas/Follower.schema'
 import Tweet from '~/models/schemas/Tweet.schema'
+import Hashtag from '~/models/schemas/Hashtag.schema'
 config()
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@twitter-dev.vxdvzen.mongodb.net/?retryWrites=true&w=majority`
@@ -73,6 +74,10 @@ class DatabaseService {
 
   get followers(): Collection<Follower> {
     return this.db.collection(process.env.DB_FOLLOWERS_COLLECTION as string)
+  }
+
+  get hashtags(): Collection<Hashtag> {
+    return this.db.collection(process.env.DB_HASHTAGS_COLLECTION as string)
   }
 }
 
