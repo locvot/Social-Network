f4a0fa09d78c197c7d4d56ba54c854b380524de0
locvot <vol6921@gmail.com>
2023-08-10 17:41:20 +0700

    encode video to HLS

--- twitter/src/utils/file.ts
+++ twitter/src/utils/file.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express'
 import { File } from 'formidable'
 import fs from 'fs'
+import path from 'path'
 import { UPLOAD_IMAGE_TEMP_DIR, UPLOAD_VIDEO_DIR, UPLOAD_VIDEO_TEMP_DIR } from '~/constants/dir'
 
 export const initFolder = () => {
@@ -43,10 +44,18 @@ export const handleUploadImage = async (req: Request) => {
   })
 }
 
+// Create unique id for video from the start
+// Process:
+// Upload video: Upload video successfully -> resolve to client
+// Encode video: Add 1 url enpoint to check video encoding status
 export const handleUploadVideo = async (req: Request) => {
   const formidable = (await import('formidable')).default
+  const nanoId = (await import('nanoid')).nanoid
+  const idName = nanoId()
+  const folderPath = path.resolve(UPLOAD_VIDEO_DIR, idName)
+  fs.mkdirSync(path.resolve(folderPath))
   const form = formidable({
-    uploadDir: UPLOAD_VIDEO_DIR,
+    uploadDir: path.resolve(folderPath),
     maxFiles: 1,
     maxFileSize: 500 * 1024 * 1024, // 50MB
     filter: function ({ name, originalFilename, mimetype }) {
@@ -55,6 +64,9 @@ export const handleUploadVideo = async (req: Request) => {
         form.emit('error' as any, new Error('File type is not valid') as any)
       }
       return valid
+    },
+    filename: function () {
+      return idName
     }
   })
   return new Promise<File[]>((resolve, reject) => {
@@ -71,6 +83,7 @@ export const handleUploadVideo = async (req: Request) => {
         const ext = getExtension(video.originalFilename as string)
         fs.renameSync(video.filepath, video.filepath + '.' + ext)
         video.newFilename = video.newFilename + '.' + ext
+        video.filepath = video.filepath + '.' + ext
       })
       resolve(files.video as File[])
     })
