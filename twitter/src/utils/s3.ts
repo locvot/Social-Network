c1d5538056eb0d1c6bafb227eeed7fe80d7b8655
locvot <vol6921@gmail.com>
2023-08-12 19:58:04 +0700

    upload file to s3

--- twitter/src/utils/s3.ts
+++ twitter/src/utils/s3.ts
@@ -1,5 +1,8 @@
 import { S3 } from '@aws-sdk/client-s3'
+import { Upload } from '@aws-sdk/lib-storage'
+import fs from 'fs'
 import { config } from 'dotenv'
+import path from 'path'
 config()
 
 const s3 = new S3({
@@ -9,4 +12,23 @@ const s3 = new S3({
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string
   }
 })
-s3.listBuckets({}).then((data) => console.log(data))
+
+const file = fs.readFileSync(path.resolve('uploads/images/2c24af7f031e1c1ea08699a00.jpg'))
+const parallelUploads3 = new Upload({
+  client: s3,
+  params: { Bucket: 'twitter-clone-6921', Key: 'image1.jpg', Body: file, ContentType: 'image/jpeg' },
+  tags: [
+    /*...*/
+  ], // optional tags
+  queueSize: 4, // optional concurrency configuration
+  partSize: 1024 * 1024 * 5, // optional size of each part, in bytes, at least 5MB
+  leavePartsOnError: false // optional manually handle dropped parts
+})
+
+parallelUploads3.on('httpUploadProgress', (progress) => {
+  console.log(progress)
+})
+
+parallelUploads3.done().then((res) => {
+  console.log(res)
+})
