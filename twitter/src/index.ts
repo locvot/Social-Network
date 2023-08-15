efc2b806974eced4514499a86fa211cb6df06f59
locvot <vol6921@gmail.com>
2023-08-15 15:21:31 +0700

    refactor socket server

--- twitter/src/index.ts
+++ twitter/src/index.ts
@@ -14,16 +14,8 @@ import searchRouter from './routes/search.routes'
 import cors from 'cors'
 import './utils/s3'
 import { createServer } from 'http'
-import { Server } from 'socket.io'
-import Conversation from './models/schemas/Conversation.schema'
 import conversationsRouter from './routes/conversations.routes'
-import { ObjectId } from 'mongodb'
-import { verifyAccessToken } from './utils/common'
-import { TokenPayload } from './models/requests/User.request'
-import { UserVerifyStatus } from './constants/enum'
-import { ErrorWithStatus } from './models/Errors'
-import { USERS_MESSAGES } from './constants/messages'
-import HTTP_STATUS from './constants/httpStatus'
+import initSocket from './utils/socket'
 // import '~/utils/fake'
 config()
 
@@ -62,86 +54,8 @@ app.use('/static', express.static(UPLOAD_VIDEO_DIR))
 // Error handler
 app.use(defaultErrorHandler)
 
-const io = new Server(httpServer, {
-  cors: {
-    origin: 'http://localhost:4000'
-  }
-})
-
-const users: {
-  [key: string]: {
-    socket_id: string
-  }
-} = {}
-
-io.use(async (socket, next) => {
-  const { Authorization } = socket.handshake.auth
-  const access_token = Authorization?.split(' ')[1]
-  try {
-    const decoded_authorization = await verifyAccessToken(access_token)
-    const { verify } = decoded_authorization as TokenPayload
-    if (verify !== UserVerifyStatus.Verified) {
-      throw new ErrorWithStatus({
-        message: USERS_MESSAGES.USER_NOT_VERIFIED,
-        status: HTTP_STATUS.FORBIDDEN
-      })
-    }
-    //
-    socket.handshake.auth.decoded_authorization = decoded_authorization
-    socket.handshake.auth.access_token = access_token
-    next()
-  } catch (error) {
-    next({
-      message: 'Unauthorized',
-      name: 'UnauthorizedError',
-      data: error
-    })
-  }
-})
-
-io.on('connection', (socket) => {
-  console.log(`user ${socket.id} connected`)
-  const { user_id } = socket.handshake.auth.decoded_authorization as TokenPayload
-  users[user_id] = {
-    socket_id: socket.id
-  }
-  console.log(users)
-  socket.use(async (packet, next) => {
-    const { access_token } = socket.handshake.auth
-    try {
-      await verifyAccessToken(access_token)
-      next()
-    } catch (error) {
-      next(new Error('Unauthorized'))
-    }
-  })
-  socket.on('error', (error) => {
-    if (error.message === 'Unauthorized') {
-      socket.disconnect()
-    }
-  })
-  socket.on('send_message', async (data) => {
-    const { receiver_id, sender_id, content } = data.payload
-    const receiver_socket_id = users[receiver_id]?.socket_id
-    const conversation = new Conversation({
-      sender_id: new ObjectId(sender_id),
-      receiver_id: new ObjectId(receiver_id),
-      content
-    })
-    const result = await databaseService.conversations.insertOne(conversation)
-    conversation._id = result.insertedId
-    if (receiver_socket_id) {
-      socket.to(receiver_socket_id).emit('receive_message', {
-        payload: conversation
-      })
-    }
-  })
-  socket.on('disconnect', () => {
-    delete users[user_id]
-    console.log(`user ${socket.id} disconnected`)
-    console.log(users)
-  })
-})
+// Initialize socket
+initSocket(httpServer)
 
 httpServer.listen(port, () => {
   console.log(`Server running on port ${port}`)
