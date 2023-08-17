808f5234bb32841932e3458432e6b23fd565be23
locvot <vol6921@gmail.com>
2023-08-17 15:52:36 +0700

    use express-rate-limit

--- twitter/src/index.ts
+++ twitter/src/index.ts
@@ -19,6 +19,7 @@ import swaggerUi from 'swagger-ui-express'
 import swaggerJSDoc from 'swagger-jsdoc'
 import { envConfig, isProduction } from './constants/config'
 import helmet from 'helmet'
+import rateLimit from 'express-rate-limit'
 // import '~/utils/fake'
 
 const options: swaggerJSDoc.Options = {
@@ -43,6 +44,14 @@ databaseService.connect().then(() => {
 })
 
 const app = express()
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false // Disable the `X-RateLimit-*` headers
+  // store: ... , // Use an external store for more precise rate limiting
+})
+app.use(limiter)
 const httpServer = createServer(app)
 
 // Enable CORS
